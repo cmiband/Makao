a1d6d79b4362dcd5a004bf9ba7494550824e8244
cmiband <bartadamo2005@gmail.com>
2021-12-20 20:53:24 +0100

    probably end of basic front-end
    also first socket.io test

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
 const express = require('express');
 const app = express();
+const server = require('http').createServer(app);
 const port = 3000;
 const path = require('path');
+const io = require('socket.io')(server);
 const favicon = require('serve-favicon');
 
 app.use(favicon(__dirname+'/public/graphics/favicon.ico'));
-app.use(express.static(__dirname+'/public'));
+app.use(express.static(__dirname));
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.join(__dirname,'/public/views/index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'views', 'index.html'))
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log("Listening on port "+port);
+});
+
+io.on('connection', socket => {
+    console.log(socket.id)
 });
